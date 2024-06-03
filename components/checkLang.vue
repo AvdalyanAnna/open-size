8b4ffa3d06b252dd3d00e03846f67f83fdc0b6c8
Anna Avdalyan <anna_avdalyan@intdevels.com>
2024-06-03 13:26:20 +0400

    fix

--- components/checkLang.vue
+++ components/checkLang.vue
@@ -113,7 +113,7 @@ const changeEng = () =>{
   <div class="flex min-h-[100vh] flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
       <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-        <div v-if="currentTime">
+        <div v-if="currentTime > 0">
           <h2 class="text-center">Можете попробовать снова четез</h2>
           <p class="text-[100px] text-center"> {{ $getMinutes(currentTime) }}</p>
         </div>
